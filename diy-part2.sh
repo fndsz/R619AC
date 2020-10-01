7a5edf7589c1908d4c110a895b8051edc007d206
fndsz <45010248+fndsz@users.noreply.github.com>
2020-10-01 19:30:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,26 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+device_name='G-DOCK'
+wifi_name='G-DOCK'
+lan_ip='192.168.1.1'        # Lan Ip地址
+utc_name='Asia\/Shanghai'   # 时区
+ 
+#修改机器名称
+echo "设置主机名"
+sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
+
+# Modify default IP
+echo "设置lan ip"
+sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+
+#修改默认时区
+echo "修改时区"
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+
+echo "修改wifi名称"
+
+sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
